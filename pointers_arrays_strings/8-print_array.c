4904b0d470e96d247eb26cad6fc7cb7d80157c37
lucasscianna <sciannalucas0@gmail.com>
2025-10-31 10:36:05 +0000

    .

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* print_array - Prints the content of an array
+* @a: Array to print
+* @n: Number of char to print
+* Return: void
+*/
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef main_h
 #define main_h
 
+void print_array(int *a, int n);
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
