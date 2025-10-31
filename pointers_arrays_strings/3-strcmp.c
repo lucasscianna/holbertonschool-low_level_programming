9f7fcab2cebfda450ec37aa28eef8a1ab71deee0
lucasscianna <sciannalucas0@gmail.com>
2025-10-31 14:08:41 +0000

    .

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+* _strcmp - Compare 2 strings
+* @s1: 1st string
+* @s2: 2nd string
+* Return: String comparision
+*/
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+
+	return (s1[i] - s2[i]);
+}
