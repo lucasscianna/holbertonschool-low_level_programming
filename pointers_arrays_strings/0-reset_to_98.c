81d60080a403ce03a7e4477ffb2bf552e2db78d8
lucasscianna <sciannalucas0@gmail.com>
2025-10-31 09:12:51 +0000

    .

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -3,11 +3,14 @@
 /**
  * reset_to_98 - uptade value for the pointer to 98.
  *
- *Description: fonction take value from the variable and edit from the memory adress
+ *Description: fonction take value from the variable
+ * and edit from the memory adress
+ *
+ *@n: is the variable
+ *
  */
 
 void reset_to_98(int *n);
 {
 *n = 98;
 }
-    
