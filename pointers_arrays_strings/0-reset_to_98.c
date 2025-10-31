ea676cbb1ca4fbcd035c69552f68b683f88e8b78
lucasscianna <sciannalucas0@gmail.com>
2025-10-31 09:09:45 +0000

    .

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - uptade value for the pointer to 98.
+ *
+ *Description: fonction take value from the variable and edit from the memory adress
+ */
+
+void reset_to_98(int *n);
+{
+*n = 98;
+}
+    
