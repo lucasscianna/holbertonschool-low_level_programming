d8c06874878f17b59bcafbef7bdf152b97a7b7f8
lucasscianna <sciannalucas0@gmail.com>
2025-11-05 09:15:02 +0000

    .

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strpbrk - Searches a string for any of a set of bytes
+ * @s: Pointer to the string to search
+ * @accept: Pointer to the string containing bytes to search for
+ *
+ * Return: Pointer to the first byte in s that matches one
+ * of the bytes in accept, or NULL if none found
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (&s[i]);
+			}
+		}
+	}
+	return (0);
+}
