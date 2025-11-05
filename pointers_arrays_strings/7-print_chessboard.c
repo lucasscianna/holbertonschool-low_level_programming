f328dc2cd5ce6bcbc0715cb367647fd5e547f373
lucasscianna <sciannalucas0@gmail.com>
2025-11-05 09:21:04 +0000

    .

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_chessboard - Prints an 8x8 chessboard
+ * @a: Pointer to a 2D array of 8x8 characters
+ *
+ * Description: Prints each row of the chessboard followed by a newline.
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}
