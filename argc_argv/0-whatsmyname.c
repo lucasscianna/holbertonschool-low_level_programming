7b08828b238f8b2a6433919925be6866addbaf09
lucasscianna <sciannalucas0@gmail.com>
2025-11-07 09:40:23 +0000

    .

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints its own name
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
