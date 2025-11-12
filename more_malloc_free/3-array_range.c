ab396516ed79dc65c4b6b7941e2cc3051ad2aa16
lucasscianna <sciannalucas0@gmail.com>
2025-11-12 13:58:21 +0000

    .

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * array_range - Allocates and init at 0 memory for an array.
+ * @min: Number of elements.
+ * @max: Size of each elements.
+ *
+ * Return: Pointer to the allocated memory, NULL on failure.
+ */
+int *array_range(int min, int max)
+{
+	int *arr, i, size;
+
+	if (min > max)
+		return (NULL);
+
+	size = max - min + 1;
+	
+	arr = malloc((max - min + 1) * (sizeof(int)));
+		if (arr == NULL)
+			return (NULL);
+
+	for (i = 0; i < size - 1; i++)
+		arr[i] = min + i;
+
+	return (arr);
+}
