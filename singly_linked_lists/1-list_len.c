e29af5276fad22ed720f3165a6c381c6a20fe907
lucasscianna <sciannalucas0@gmail.com>
2025-12-02 12:22:52 +0000

    .

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+*list_len - Return the numbers of elements in a linked list
+*@h: the list_t list
+*
+*Return: the numbers of elements in the list
+*
+*/
+
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
