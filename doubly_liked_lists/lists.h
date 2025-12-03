d9d58987e0a236f990814dcd9e4f1afe342001cd
lucasscianna <sciannalucas0@gmail.com>
2025-12-03 09:47:10 +0000

    .

---
+++ doubly_liked_lists/lists.h
@@ -0,0 +1,24 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * struct listint_s - Double linked list
+ * @n: Interger.
+ * @prev: Points to previous node.
+ * @next: Points to the next node.
+ */
+typedef struct dlistint_s
+{
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
+
+size_t print_dlistint(const dlistint_t *h);
+
+#endif
+
