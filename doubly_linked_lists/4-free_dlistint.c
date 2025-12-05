3fc3f96870fdd696bcd4d39f51a266874550cd79
lucasscianna <sciannalucas0@gmail.com>
2025-12-05 08:46:05 +0000

    .

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+*free_dlistint - function that frees a dlistint_t list
+*@head: pointer to the head of the dlistint_t list
+*
+*/
+
+void free_dlistint(dlistint_t *head)
+{
+dlistint_t *actual;
+
+while (head != NULL)
+{
+actual = head->next;
+free(head);
+head = actual;
+}
+
+}
